17893f79c5fd83b4c4388de49575369cd5277d30
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2025-04-11 15:42:02 -0300

    Updating cleaning methods and adding comments

--- src/receiver.c
+++ src/receiver.c
@@ -1,14 +1,16 @@
 #include "shared_memory.h"
 
-shm_buf_t *shm_buf = NULL;
-int shm_fd = -1;
-size_t buf_size = sizeof(shm_buf_t);
-
 int main()
 {
+    // Shared memory variables
+    shm_buf_t *shm_buf = NULL;
+    int shm_fd = -1;
+    size_t buf_size = sizeof(shm_buf_t);
 
-    sem_t *sem_shm = sem_open("/sem_shm", O_CREAT, 0644, 1);
+    // Semaphore variable
+    sem_t *sem_shm;
 
+    sem_shm = sem_open("/sem_shm", O_CREAT, 0644, 1);
 
     // Create shared memory resource
     shm_fd = shm_open("/shm_buf", O_RDWR | O_CREAT, 0644);
@@ -20,19 +22,23 @@ int main()
     shm_buf = mmap(0, buf_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
     // Now we can access the shared memory segment directly from shm_buf
+    // Trying to get the semaphore and waiting until it occurs
+    if (sem_wait(sem_shm) == 0)
+    {
+        // Semaphore picked!
 
-    if(sem_wait(sem_shm) == 0){
+        printf("%s\n", shm_buf->string);
 
-        printf("%s\n",shm_buf->string);
-
+        // Realaesing the semaphore
         sem_post(sem_shm);
     }
 
-
-
+    // Cleaning semaphore stuff
     sem_close(sem_shm);
     sem_unlink("/sem_shm");
 
-    return 0;
+    // Cleaning shared memory stuff (deletes the shared section)
+    // shm_unlink("/shm_buf");
 
+    return 0;
 }
